f38ed2fc15b05faf61ceb966232be65419746cf0
Miaofei Mei <miaofei@amazon.com>
2020-06-02 13:22:36 -0700

    update colcon packages (#15)
    Signed-off-by: Miaofei <miaofei@amazon.com>

--- setup-ros.sh
+++ setup-ros.sh
@@ -71,26 +71,26 @@ apt-get install --no-install-recommends --quiet --yes libopensplice69 || true
 pip3 install --upgrade \
 	argcomplete \
 	catkin_pkg \
-	colcon-bash==0.4.1 \
+	colcon-bash==0.4.2 \
 	colcon-cd==0.1.1 \
-	colcon-cmake==0.2.16 \
+	colcon-cmake==0.2.22 \
 	colcon-common-extensions==0.2.1 \
-	colcon-core==0.4.3 \
-	colcon-defaults==0.2.2 \
-	colcon-lcov-result==0.2.0 \
+	colcon-core==0.5.9 \
+	colcon-defaults==0.2.5 \
+	colcon-lcov-result==0.4.0 \
 	colcon-library-path==0.2.1 \
-	colcon-metadata==0.2.2 \
-	colcon-mixin==0.1.6 \
-	colcon-notification==0.2.10 \
-	colcon-output==0.2.6 \
-	colcon-package-information==0.3.0 \
-	colcon-package-selection==0.2.5 \
+	colcon-metadata==0.2.4 \
+	colcon-mixin==0.1.8 \
+	colcon-notification==0.2.13 \
+	colcon-output==0.2.9 \
+	colcon-package-information==0.3.3 \
+	colcon-package-selection==0.2.6 \
 	colcon-parallel-executor==0.2.4 \
 	colcon-pkg-config==0.1.0 \
 	colcon-powershell==0.3.6 \
-	colcon-python-setup-py==0.2.2 \
-	colcon-recursive-crawl==0.2.0 \
-	colcon-ros==0.3.13 \
+	colcon-python-setup-py==0.2.5 \
+	colcon-recursive-crawl==0.2.1 \
+	colcon-ros==0.3.17 \
 	colcon-test-result==0.3.8 \
 	coverage \
 	cryptography \
